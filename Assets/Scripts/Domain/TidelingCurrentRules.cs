d9fd9d52cab95acbad33d0d116a728261b31bafd
Quill Kirby <quillkirby26@gmail.com>
2026-08-04 13:33:00 -0400

    Improve Current journal accessibility

--- Assets/Scripts/Domain/TidelingCurrentRules.cs
+++ Assets/Scripts/Domain/TidelingCurrentRules.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 namespace Tidepool.Domain
 {
     public static class TidelingCurrentRules
@@ -56,6 +58,44 @@ namespace Tidepool.Domain
                     return "Tide";
                 default:
                     return current.ToString();
+            }
+        }
+
+        public static string GetIconName(TidelingCurrent current)
+        {
+            switch (current)
+            {
+                case TidelingCurrent.Current:
+                    return "Wave";
+                case TidelingCurrent.Coral:
+                    return "Coral Branch";
+                case TidelingCurrent.Stone:
+                    return "Pebble";
+                case TidelingCurrent.Glow:
+                    return "Lantern";
+                case TidelingCurrent.Tide:
+                    return "Shell";
+                default:
+                    return "Current Mark";
+            }
+        }
+
+        public static Color GetDisplayColor(TidelingCurrent current)
+        {
+            switch (current)
+            {
+                case TidelingCurrent.Current:
+                    return new Color32(45, 132, 184, 255);
+                case TidelingCurrent.Coral:
+                    return new Color32(219, 108, 119, 255);
+                case TidelingCurrent.Stone:
+                    return new Color32(118, 113, 103, 255);
+                case TidelingCurrent.Glow:
+                    return new Color32(215, 165, 55, 255);
+                case TidelingCurrent.Tide:
+                    return new Color32(58, 150, 139, 255);
+                default:
+                    return Color.white;
             }
         }
     }
