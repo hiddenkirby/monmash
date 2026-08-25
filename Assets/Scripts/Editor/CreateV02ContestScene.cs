5ae0ca2b700b3f0bb37b0021aea758d20298de1e
Quill Kirby <quillkirby26@gmail.com>
2026-08-25 12:08:45 -0400

    Add contest Current ring display

--- Assets/Scripts/Editor/CreateV02ContestScene.cs
+++ Assets/Scripts/Editor/CreateV02ContestScene.cs
@@ -53,6 +53,17 @@ namespace Tidepool.Editor
             Text visitingStatus = CreateText("VisitingStatus", visitingPanel.transform, string.Empty, 22, TextAnchor.MiddleCenter, new Vector2(0f, -160f), new Vector2(300f, 40f));
             visitingStatus.color = new Color(0.5f, 0.5f, 0.5f);
 
+            Image currentRingPanel = CreateImage("CurrentRingPanel", safeArea, new Color(0.95f, 0.98f, 0.94f, 0.92f), new Vector2(0f, 318f), new Vector2(360f, 104f));
+            Image[] currentRingNodes = new Image[5];
+            Text[] currentRingLabels = new Text[5];
+            CreateCurrentRingNode(currentRingPanel.transform, 0, "Current", new Vector2(0f, 28f), new Color(0.18f, 0.52f, 0.72f), currentRingNodes, currentRingLabels);
+            CreateCurrentRingNode(currentRingPanel.transform, 1, "Coral", new Vector2(86f, 8f), new Color(0.86f, 0.42f, 0.47f), currentRingNodes, currentRingLabels);
+            CreateCurrentRingNode(currentRingPanel.transform, 2, "Stone", new Vector2(52f, -30f), new Color(0.46f, 0.44f, 0.40f), currentRingNodes, currentRingLabels);
+            CreateCurrentRingNode(currentRingPanel.transform, 3, "Glow", new Vector2(-52f, -30f), new Color(0.84f, 0.65f, 0.22f), currentRingNodes, currentRingLabels);
+            CreateCurrentRingNode(currentRingPanel.transform, 4, "Tide", new Vector2(-86f, 8f), new Color(0.23f, 0.59f, 0.55f), currentRingNodes, currentRingLabels);
+            Text currentAdvantageText = CreateText("CurrentAdvantageText", currentRingPanel.transform, "Currents are even.", 18, TextAnchor.MiddleCenter, new Vector2(0f, -48f), new Vector2(320f, 26f));
+            currentAdvantageText.color = new Color(0.08f, 0.18f, 0.22f);
+
             Text roundCounterText = CreateText("RoundCounterText", safeArea, "Round 1 of 3 - You 0, Visitor 0", 26, TextAnchor.MiddleCenter, new Vector2(0f, -20f), new Vector2(720f, 48f));
             roundCounterText.color = new Color(0.08f, 0.18f, 0.22f);
 
@@ -84,6 +95,9 @@ namespace Tidepool.Editor
             serializedController.FindProperty("retryButton").objectReferenceValue = retryButton;
             serializedController.FindProperty("exitButton").objectReferenceValue = exitButton;
             serializedController.FindProperty("exitSceneName").stringValue = "Overworld";
+            SetObjectReferenceArray(serializedController.FindProperty("currentRingNodes"), currentRingNodes);
+            SetObjectReferenceArray(serializedController.FindProperty("currentRingLabels"), currentRingLabels);
+            serializedController.FindProperty("currentAdvantageText").objectReferenceValue = currentAdvantageText;
             serializedController.ApplyModifiedProperties();
 
             EditorSceneManager.SaveScene(scene, ScenePath);
@@ -123,6 +137,16 @@ namespace Tidepool.Editor
             return button;
         }
 
+        private static void CreateCurrentRingNode(Transform parent, int index, string label, Vector2 anchoredPosition, Color color, Image[] nodes, Text[] labels)
+        {
+            Image node = CreateImage($"{label}CurrentNode", parent, color, anchoredPosition, new Vector2(76f, 32f));
+            Text text = CreateText("Label", node.transform, label, 14, TextAnchor.MiddleCenter, Vector2.zero, new Vector2(76f, 32f));
+            text.color = Color.white;
+
+            nodes[index] = node;
+            labels[index] = text;
+        }
+
         private static Image CreateImage(string name, Transform parent, Color color, Vector2 anchoredPosition, Vector2 size)
         {
             RectTransform rectTransform = CreateRect(name, parent);
@@ -157,6 +181,15 @@ namespace Tidepool.Editor
             rectTransform.anchorMax = new Vector2(0.5f, 0.5f);
             rectTransform.pivot = new Vector2(0.5f, 0.5f);
             return rectTransform;
+        }
+
+        private static void SetObjectReferenceArray(SerializedProperty property, Object[] values)
+        {
+            property.arraySize = values.Length;
+            for (int i = 0; i < values.Length; i++)
+            {
+                property.GetArrayElementAtIndex(i).objectReferenceValue = values[i];
+            }
         }
 
         private static Font GetBuiltinFont()
